b9e330c88f22c1ab2be96fdf8d75a4e86c9d1b29
이윤서 <yyoo04@naver.com>
2025-07-07 20:10:43 +0900

    SearchBar 컴포넌트

---
+++ likelion_week05/src/pages/SignIn.jsx
@@ -0,0 +1,14 @@
+import Info from "../components/Info";
+
+const SignIn = () => {
+    return (
+        <>
+            <div className="flex justify-center mt-[24px]">
+                <Info />
+            </div>
+            
+        </>
+    )
+}
+
+export default SignIn;
