db900aaa83e0648653ce7751449f5723e63ab32f
Juyoung03 <iris031008@naver.com>
2025-07-09 12:57:55 +0900

    feat: shoppingCart 페이지 구현

--- likelion_week05/src/App.jsx
+++ likelion_week05/src/App.jsx
@@ -1,5 +1,6 @@
 import Main from "./pages/Main";
 import SignIn from "./pages/Signin";
+import ShoppingCart from "./pages/ShoppingCart";
 import Layout from "./components/layouts/Layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />}/>
           <Route path="/signin" element={<SignIn />}/>
+          <Route path="/shoppingCart" element={<ShoppingCart />} />
         </Route>
       </Routes>
     </BrowserRouter>
