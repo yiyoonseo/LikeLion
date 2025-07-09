3202dd947cee79648181618deced4d2bb0336615
이윤서 <yyoo04@naver.com>
2025-07-09 12:23:59 +0900

    상품 상세 조회 페이지

--- likelion_week05/src/App.jsx
+++ likelion_week05/src/App.jsx
@@ -1,6 +1,7 @@
 import Main from "./pages/Main";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import Layout from "./components/layouts/Layout";
+import ProductsDetail from "./pages/ProductsDetail";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Main />}/>
-          <Route path="/signin" element={<SignIn />}/>
+          <Route index element={<Main />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/products/:id" element={<ProductsDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
